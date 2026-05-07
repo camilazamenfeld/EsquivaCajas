ceece126f5a7ff9c99e9ce9804bc0b4a77329b27
camilazamenfeld <camizamenfeld@gmail.com>
2026-05-07 10:20:05 -0300

    .

---
+++ Assets/Scripts/CajaMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaMovement : MonoBehaviour
+{
+    public float speed = 2f;
+
+
+    void Update()
+    {
+       
+        
+        transform.Translate(0, -speed * Time.deltaTime, 0);
+
+        
+    }
+}
+
