71ac40aea291ffa387cbad79a5bf7053056d703e
camilazamenfeld <camizamenfeld@gmail.com>
2026-05-07 11:46:52 -0300

    caja vuelve a su lugar inicial

---
+++ Assets/Scripts/CajaCollisionDetection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaCollisionDetection : MonoBehaviour
+{
+    public CajaMovement cajaMovementScript;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cajaMovementScript = GetComponent<CajaMovement>();
+        
+    }
+
+
+      void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Piso"))
+        {
+            cajaMovementScript.MoveBoxToRandomPosition();
+        }
+    }
+}
--- Assets/Scripts/CajaMovement.cs
+++ Assets/Scripts/CajaMovement.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class CajaMovement : MonoBehaviour
 {
     public float speed = 2f;
+    float initialYValue;
 
+void Start()
 
+{
+    initialYValue = transform.position.y;
+}
     void Update()
     {
        
@@ -14,6 +19,14 @@ public class CajaMovement : MonoBehaviour
         transform.Translate(0, -speed * Time.deltaTime, 0);
 
         
+    }
+
+    public void MoveBoxToRandomPosition()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.y = initialYValue;
+        transform.position = newPosition;
+
     }
 }
 
