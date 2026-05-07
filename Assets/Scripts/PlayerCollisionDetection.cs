aa2d2c1c4cdb7937726fc4b1cbc174e45dbd52fb
camilazamenfeld <camizamenfeld@gmail.com>
2026-05-07 10:35:04 -0300

    player collision

---
+++ Assets/Scripts/PlayerCollisionDetection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionDetection : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Caja"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
