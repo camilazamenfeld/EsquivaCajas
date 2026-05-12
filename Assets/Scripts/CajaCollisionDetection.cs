f9d82bbc92e3ff85616b5068567c93051669db2e
camilazamenfeld <camizamenfeld@gmail.com>
2026-05-12 14:31:36 -0300

    termine

--- Assets/Scripts/CajaCollisionDetection.cs
+++ Assets/Scripts/CajaCollisionDetection.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class CajaCollisionDetection : MonoBehaviour
 {
     public CajaMovement cajaMovementScript;
-    
-    // Start is called before the first frame update
+    public UITimer timerScript;
+
     void Start()
     {
         cajaMovementScript = GetComponent<CajaMovement>();
-        
     }
 
-
-      void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Piso"))
         {
+            cajaMovementScript.speed += 1f;
             cajaMovementScript.MoveBoxToRandomPosition();
+        }
+
+        if (collision.gameObject.CompareTag("Caja"))
+        {
+            timerScript.juegoTerminado = true;
         }
     }
 }
